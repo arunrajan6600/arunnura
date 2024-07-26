e247d0f65164217f7dfd7b900b8c2ec348151fb1
Vivek <vivekkevivo@gmail.com>
2024-07-26 22:34:34 +0530

    chore: setup github-action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/arunnura",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
