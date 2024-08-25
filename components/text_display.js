133a84bbb5c707d144b390ee9e058a3351d95b9f
Vivek <vivekkevivo@gmail.com>
2024-08-25 11:31:38 +0530

    feat: parse instagram url and display post

--- components/text_display.js
+++ components/text_display.js
@@ -1,14 +1,25 @@
 
+import InstaPost from "@/components/insta_post";
+
+
 const TextDisplay = ({ content }) => {
   
   const sections = content.split('\n\n');
+
+  const findInstagramUrl = (text) => {
+    const instagramRegex = /https:\/\/www\.instagram\.com\S*/g;
+    const matches = text.match(instagramRegex);
+  
+    // Return the first match or null if no match is found
+    return matches ? matches[0] : null;
+  };
 
   return (
     <div>
       {sections.map((section, sectionIndex) => (
         <div key={sectionIndex}>
           {section.split('\n').map((line, lineIndex) => (
-            <h2 key={lineIndex}>{line}</h2>
+            findInstagramUrl(line) ? <InstaPost key={lineIndex} shareLink={findInstagramUrl(line)}></InstaPost> : <h2 key={lineIndex}>{line}</h2>
           ))}
           {sectionIndex < sections.length - 1 && <h3>&nbsp;</h3>}
         </div>
